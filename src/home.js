1c353e0ff5b9f5b3ada8d455678d84b9c2edace6
Josh Sherman <joshua.v.sherman@gmail.com>
2017-04-03 12:19:46 -0400

    resizing of home page

--- src/home.js
+++ src/home.js
@@ -1,14 +1,27 @@
 //import {computedFrom} from 'aurelia-framework';
-import {Router} from 'aurelia-router';
-import {inject} from 'aurelia-framework';
+//import {Router, Bindable} from 'aurelia-router';
+//import {inject} from 'aurelia-framework';
 
-@inject(Router)
+//@inject(Router)
 export class Home {
   
-  constructor(router) {
-    this.router = router;
-  }
+  //constructor(router) {
+    //this.router = router;
+  //}
   
+  
+  get widescreen(){
+    let iswidescreen = false;
+    let currentscreenwidth = document.documentElement.clientWidth;
+    /* istanbul ignore else */
+    if (currentscreenwidth > 885){
+      iswidescreen = true;
+      //this.columnWidth = '450px';
+    } //else {
+      //this.columnWidth = 'auto';
+    //}
+    return iswidescreen;
+  }
   // attached() {
   //     document.title = this.router.currentInstruction.config.title;
   // }
