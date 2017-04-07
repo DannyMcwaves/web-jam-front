82f9e7245d04535952e57a0b0ca76717d15cab94
DannyMcwaves <dannymcwaves96@rocketmail.com>
2017-04-07 16:57:54 +0000

    login test fixed wth 100% coverage

--- test/unit/login.spec.js
+++ test/unit/login.spec.js
@@ -21,24 +21,49 @@ class AppStub {
   authenticated = false;
 }
 
+class AppState {
+
+  constructor() {
+    this._auth = false;
+    this._roles = [];
+  }
+
+  setAuth(value) {
+    this._auth = value;
+  }
+
+  setRoles(roles) {
+    this._roles = roles;
+  }
+
+}
+
+class Router {
+  currentInstruction = { config: { title: 'Howdy is cool' } }
+}
+
 describe('the Login module', () => {
   var sut;
   var app1;
   var auth;
-  
+  var router;
+  var appState;
+
   beforeEach(() => {
     app1 = new AppStub();
     auth = new AuthStub();
-    sut = new Login(auth, app1);
+    router = new Router(),
+    appState = new AppState();
+    sut = new Login(auth, app1, router, appState);
   });
-  
+
   it('should expect authentication to function as rewritten.', done => {
     sut.authenticate('google').then((data) => {
       //console.log(data); // disable this if you want to.
       done();
     }, null);
   });
-  
+
   it('runs the authenticate function', (done) => {
     sut.authenticate('google');
     //expect isAuthenticated to be called after the sut.authenticate is done calling to register change in authentication.
@@ -47,4 +72,12 @@ describe('the Login module', () => {
       done();
     }, 5);
   });
+
+  it('should be attached to router', done => {
+      sut.attached();
+      console.log(sut.title);
+      expect(sut.title).toBe('Howdy is cool');
+      done()
+  });
+
 });
